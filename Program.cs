03d79c966f551b35f88f7e4dac58201e38cb36b8
Home Sweet <vyacheslav.churaev.1901@yandex.ru>
2024-10-27 11:50:15 +0300

    Initial Project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        var numbers = new[] { 34, 15, 88, 2 };
+
+        var minNUmber = numbers.Min(x => x);
+        Console.WriteLine(minNUmber);
+    }
+}
